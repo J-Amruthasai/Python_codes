fcc71b9fcd7103e206dc730121c16043fa313c7e
J-Amruthasai <105196917+J-Amruthasai@users.noreply.github.com>
2024-06-30 16:05:32 +0530

    Update Blackjack program.py

--- Blackjack program.py
+++ Blackjack program.py
@@ -1,82 +1,137 @@
-import random
+# Blackjack Capstone Project
 
-# Q - 10
-# K - 10
-# J - 10
-#total should should be less than 21 and if sum is less than 17 than take other card or else only 2 card
-#[11 or 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+Blacjack_capstone_logo = '''
+ _     _            _    _            _    
+| |   | |          | |  (_)          | |   
+| |__ | | __ _  ___| | ___  __ _  ___| | __
+| '_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
+| |_) | | (_| | (__|   <| | (_| | (__|   < 
+|_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\\
+                       _/ |                
+                      |__/  
 
 
-def deal_card():
-    ''' choses the random card'''
-    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    card = random.choice(cards)
-    return card
+          _____
+         |X .  | _____
+         | /.\ ||X ^  | _____
+         |(_._)|| / \ ||X _  | _____
+         |  |  || \ / || ( ) ||X_ _ |
+         |____X||  .  ||(_'_)||( v )|
+                |____X||  |  || \ / |
+                       |____X||  .  |
+                              |____X|
 
-def calculate_score(cards):
-    if sum(cards) == 21 and len(cards) == 2:
-        return 0
-    if 11 in cards and sum(cards) > 21:
-        cards.remove(11)
-        cards.append(1)
-    return sum(cards)
+                                    
+                      '''
 
-def compare(user_score, computer_score):
-    if user_score == computer_score:
-        return "Draw"
-    elif computer_score == 0:
-        return " Lose, opponent has Blackjack"
-    elif user_score == 0:
-        return " Win with a Blackjack"
-    elif user_score > 21:
-        return "You went over. You lose"
-    elif computer_score > 21:
-        return "Opponent went over. You win"
-    elif user_score > computer_score:
-        return "You win"
-    else:
-        return "You lose"
-    
+
+
+
+import random , os
 def play_game():
-    is_game_over = False
-    user_cards = []
-    computer_cards = []
+    print(Blacjack_capstone_logo)
+    cards = ["11","2","3","4","5","6","7","8","9","10","10","10","10"]
+    dict= {"flowers": cards, "leaves": cards,"diamonds":cards,"hearts":cards}
 
-    for _ in range(2):
-        user_cards.append(deal_card())
-        computer_cards.append(deal_card())
+    computer = {}
+    computer_dup = {}
+    player = {}
+    player_dup = {}
+    player_score = 0
+    computer_score = 0
+    i=0
 
-    while not is_game_over:
+    def define_card(dictionary,i):
+        result_dict={}
+        for key in dictionary:
+            result_dict[key+str(i)]=random.choice(dictionary[key])
+            i=i+1
+        return result_dict, i
 
-        user_score = calculate_score(user_cards)
-        computer_score = calculate_score(computer_cards)
-        print(f" Your cards: {user_cards}, current score: {user_score}")
-        print(f" Computer's first card: {computer_cards[0]}")
 
-        if user_score == 0 or computer_score == 0 or user_score> 21:
-            is_game_over = True
-        else:
-            user_should_take = input("Type 'y' to get another card, type 'n' to pass :")
-            if (user_should_take).lower() == 'y':
-                user_cards.append(deal_card())
-            else:
-                is_game_over = True
 
-    while computer_score != 0 and computer_score < 17:
-        computer_cards.append(deal_card())
-        computer_score = calculate_score(computer_cards)
+    def selecting_keys(dictionary,k):
+        selected_keys = random.sample(dictionary.keys(),k)
+        result_dict = {key:dictionary[key] for key in selected_keys}
+        return result_dict
+
+    def extra(dictionary):
+        extra_card_dict = {}
+        extra_card_dict_key= random.choice(list(dictionary.keys()))
+        extra_card_dict_value=dictionary.pop(extra_card_dict_key)
+        extra_card_dict[extra_card_dict_key]=extra_card_dict_value
+        return extra_card_dict
+
+
+    def create_list(dictionary):
+        list = []
+        for key in dictionary:
+            list.append(dictionary[key])
+        return list
+
+    def sum_dict(dictionary):
+        add = sum(int(value) for value in dictionary.values())
+        return add
+
+    def print_fun(dictionary, string):
+        print(f"The {string} cards are:")
+        print("{")
+        for keys in dictionary:
+            key = keys.rstrip('0123456789')
+            print(f"{key}:{dictionary[keys]},",end=" ")
+        print("\n}\n")
+
+    computer_dup,j =define_card(dict,i)
+    player_dup,_ = define_card(dict,j)
+
+    computer = selecting_keys(computer_dup,3)
+    player = selecting_keys(player_dup,2)
+
+    extra_card_dict = extra(computer)
+
+    list1=create_list(player)
+    print(f"The cards you got :{list1} ")
+
+    list2=create_list(computer)
+    print(f"one card of dealer is {random.choice(list2)}")
+
+    want = input("Do you want to take a card from the dealer (y/n): ").lower()
+
+    if want == "y":
+        player.update(extra_card_dict)
+
+    player_score=sum_dict(player)
+    computer_score=sum_dict(computer)
+
+    if (str(11) in player.values() and player_score>21):
+        player_score -= 10
+
+    print_fun(player,"User")
+    print_fun(computer,"Dealer")
+
+    print(f"User score is : {player_score}")
+    print(f"Dealer score is: {computer_score}")
+
+    if player_score >21:
+        print("You busted! Dealer wins")
+    elif (player_score<computer_score):
+        print("Dealer wins")
+    elif (player_score>computer_score):
+        print("You win")
+    else:
+        print("It's a draw")
         
-    print(f" Your final hand: {user_cards}, final score: {user_score}")
-    print(f" Computer's final hand: {computer_cards}, final score: {computer_score}")
-    print(compare(user_score, computer_score))
+def clear_screen():
+    # Clear the console
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-while input("Do you want to play a game of Blackjack? Type 'y' or 'n' : ").lower()=='y':
+def main():
+    while True:
+        play_game()
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != 'yes':
+            break
+        clear_screen()
 
-    play_game()
-    
-
-
-
-
-
-    
+if __name__ == "__main__":
+    main()
