8eca8c7f90230a7c81488fcdef8ebd6bbdfc0125
J-Amruthasai <105196917+J-Amruthasai@users.noreply.github.com>
2024-06-30 16:06:41 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,7 +1,23 @@
+# Day-7 Start
+# Hang Man project
+
 
 import random
+import os
+Hangman = '''
+                                                                              
+88                                                                            
+88                                                                            
+88                                                                            
+88,dPPYba,  ,adPPYYba, 8b,dPPYba,   ,adPPYb,d8 88,dPYba,,adPYba,  ,adPPYYba, 8b,dPPYba, 
+88P'    "8a ""     `Y8 88P'   `"8a a8"    `Y88 88P'   "88"    "8a ""     `Y8 88P'   `"8a 
+88       88 ,adPPPPP88 88       88 8b       88 88      88      88 ,adPPPPP88 88       88  
+88       88 88,    ,88 88       88 "8a,   ,d88 88      88      88 88,    ,88 88       88
+88       88 `"8bbdP"Y8 88       88  `"YbbdP"Y8 88      88      88 `"8bbdP"Y8 88       88
+                                    aa,    ,88                                
+                                     "Y8bbdP"                                 '''
 
-Stages = ['''
+HANGMANPICS = ['''
   +---+
   |   |
       |
@@ -52,47 +68,65 @@ Stages = ['''
       |
 =========''']
 
+#Word bank of animals
+words = ('ant baboon badger bat bear beaver camel cat clam cobra cougar '
+         'coyote crow deer dog donkey duck eagle ferret fox frog goat '
+         'goose hawk lion lizard llama mole monkey moose mouse mule newt '
+         'otter owl panda parrot pigeon python rabbit ram rat raven '
+         'rhino salmon seal shark sheep skunk sloth snake spider '
+         'stork swan tiger toad trout turkey turtle weasel whale wolf '
+         'wombat zebra ').split()
 
+def play_game():
+    print(Hangman)
+    life = len(HANGMANPICS)
+    word_choosen = random.choice(words)
+    length = len(word_choosen)
+    #print(word_choosen)
+    list = []
+    for i in range(length):
+        list.append("_")
+    print(" ".join(list))
+    print("\n")
+    while life>=0:
+        
+        if "_" in list and life!=0:
+            guess = input("Guess a letter: ").lower()
+        elif life == 0 :
+            print("You lost the game")
+        else:
+            life = -1
+            print("Congratulations! You won the game")
+        if not guess.isalpha() or len(guess) != 1:
+            print("Invalid input. Please guess a single letter.")
+            continue
+        if guess in word_choosen:
+            for i in range(len(word_choosen)):
+                if word_choosen[i] == guess:
+                    list[i] = guess
+            if (life>0):
+                print("Good guess!")
+                print(" ".join(list))
+        else:
+            life -= 1
+            if (life >= 0):
+                print(HANGMANPICS[len(HANGMANPICS)-life-1])
+                print("You loose a life")
 
-word_list = ["ardvark","baboon","camel"]
-chosen_word = random.choice(word_list)
-lives = 6
-print("Solution for the game is "+ chosen_word)
-k=0
-length=len(chosen_word)
-word = []
-for i in range (0,length):
-    underscore = '_'
-    word.append(underscore)
+def clear_screen():
+    # Clear the console
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-end_of_game = False
-while (end_of_game == False):
-    guess = input("Make a guess of a letter in the word: ").lower()
-    if guess in word:
-        print("You already guessed this try other letter")
-    for i in range (0,length):
-        if chosen_word[i] == guess:
-            word[i] = guess
-            print("Right")
-    print(Stages[7-lives])
-    if guess not in chosen_word:
-        print("You guessed a letter which is not in the word. You loose a life")
-        lives -=1
-        if lives ==0:
-            end_of_game = True
-            print('you lose')
-    print(word)
-    if '_'  not in word:
-        end_of_game = True
-        print("you win")
+def main():
+    while True:
+        play_game()
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != 'yes':
+            break
+        clear_screen()
 
-#print ASCII stages of hangman
-    
-
-
-
-
-
+if __name__ == "__main__":
+    main()
 
 
 
